e7629c6fa081816ddeafedd874cc60ed54dae797
Hector Flores <hecto932@gmail.com>
2018-02-06 13:02:16 -0600

    Creando componentes en ReactJS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hola mundo!')
+import React from 'react'
+import { render } from 'react-dom'
+
+// Components
+import Media from './src/playlist/components/media'
+
+const app = document.getElementById('app')
+
+render(<Media />, app)
---
+++ src/playlist/components/media.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Media extends Component {
+  render () {
+    return (
+      <div>
+        <div>
+          <img
+            src=""
+            alt=""
+            width={260}
+            height={160}
+          />
+          <h3>¿Por que aprender ReactJS?</h3>
+          <p>Hector Flores</p>
+        </div> 
+      </div>
+    )
+  }
+}
+
+export default Media
