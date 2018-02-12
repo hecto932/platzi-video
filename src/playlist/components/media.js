119d055b59fc131d87669a6962d43556bce60513
Hector Flores <hecto932@gmail.com>
2018-02-11 19:40:56 -0600

    Enlazando eventos del DOM

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -3,9 +3,18 @@ import PropTypes from 'prop-types'
 import './media.css'
 
 class Media extends Component {
+  // constructor (props) {
+  //   super(props)
+  //   this.handleClick = this.handleClick.bind(this)  
+  // }
+
+  handleClick = (event) => {
+    console.log(this.props.image)
+  }
+
   render () {
     return (
-      <div className="Media">
+      <div className="Media" onClick={this.handleClick}>
         <div className="Media-cover">
           <img
             className="Media-image"
