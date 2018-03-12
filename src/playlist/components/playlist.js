f41bf0fbcfef9453baa144cb27c009caf5f48461
Hector Flores <hecto932@gmail.com>
2018-03-12 13:18:46 -0600

    Composicion de componentes

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Media from './media.js'
 import './playlist.css'
+import Play from '../../icons/components/play'
+import Volume from '../../icons/components/volume'
+import FullScreen from '../../icons/components/full-screen'
+import Pause from '../../icons/components/pause'
 
-class Playlist extends Component {
-  render () {
-    const playlist = this.props.data.categories[0].playlist
-    console.log(this.props.data)
-    return (
-      <div className="Playlist">
-        {
-          playlist.map((item, key) => {
-            return <Media {...item} key={item.id}/>
-          })
-        }
-      </div>
-    )
-  }
+function Playlist (props) {
+  const categories = props.data.categories
+  console.log(props.data)
+  return (
+    <div className="Playlist">
+      <Play 
+        size={32}
+        color="red"
+      />
+      <Volume 
+        size={32}
+        color="blue"
+      />
+      <FullScreen
+        size={32}
+        color="green"
+      />
+      <Pause
+        size={32}
+        color="green"
+      />
+      {
+        categories.map( c => {
+          return (
+            <div className="playlist-category">
+              <p className="playlist-title"><strong>{c.title}</strong></p>
+              <div className="playlist-videos">
+                {
+                  c.playlist.map(v => {
+                    return <Media {...v} key={v.id}/>
+                  })
+                }
+              </div>
+            </div> 
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default Playlist
