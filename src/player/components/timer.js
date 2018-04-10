16f786d362bd5210f61f1118d94efa1758cef063
Hector Flores <hecto932@gmail.com>
2018-04-10 16:12:07 -0500

    Duracion del video

---
+++ src/player/components/timer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './timer.css'
+
+const Timer = (props) => (
+  <div className="Timer">
+    <p>
+      <span>00 / {props.duration}</span>
+    </p>
+  </div>
+)
+
+export default Timer
